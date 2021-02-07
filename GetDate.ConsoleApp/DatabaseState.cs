b04eac8feb4cf7625d81f3e6971212f0ccc36f8a
seangnewman <seangnewman@msn.com>
2021-02-07 08:26:43 -0700

    Garbage Collection

---
+++ GetDate.ConsoleApp/DatabaseState.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GetDate.ConsoleApp
+{
+    class DatabaseState : IDisposable
+    {
+        protected SqlConnection _connection;
+        private bool _disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);                          // In dispose call
+            GC.SuppressFinalize(this);      // Let GC know that cleaned up resource
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Best practice
+            //  Send a booling to indicate the resource is being disposed
+            if (_disposed)
+            {
+
+                // The object was previously disposed
+                return;
+            }
+
+            if (disposing)
+            {
+                if (_connection != null)
+                {
+                    _connection.Dispose();
+                    _connection = null;
+
+                }
+                _disposed = false;
+            }
+        }
+
+        public virtual string GetDate()
+        {
+            // Check if the object has been disposed
+            if (_disposed)
+            {
+                throw new ObjectDisposedException("DatabaseState");
+            }
+
+
+            if (_connection == null)
+            {
+               
+                var connectionString = ConfigurationManager.ConnectionStrings["master"];
+                _connection = new SqlConnection(connectionString.ConnectionString);
+                _connection.Open();
+            }
+
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "Select getdate()";
+                return command.ExecuteScalar().ToString();
+            }
+        }
+
+         
+    }
+}
---
+++ GetDate.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDate.ConsoleApp
+{
+    class Program
+    {
+       // private static DatabaseState _DatabaseState;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("'g' to Get Date; 'gc' to Garbage Collect;  'x' to Exit");
+            var command = "";
+
+            while (command != "x")
+            {
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "g":
+                        GetDate();
+                        break;
+                    case "gc":
+                        GC.Collect();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void GetDate()
+        {
+            using (var databaseState = new DatabaseState())
+            {
+                Console.WriteLine(databaseState.GetDate());
+            }
+        }
+    }
+}
